635ff9fc4b5044279392b7d9d6a3a9686732ef07
Łukasz Łosieczka <lukasz.losieczka@gmail.com>
2024-05-05 21:21:41 +0200

    fixes

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -19,11 +19,11 @@ TARGET_IMAGE_SIZE = (224, 224)
 BATCH_SIZE = 32
 
 EXPAND_DATAGEN = tf.keras.preprocessing.image.ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=0.1,
-    zoom_range=0.1,
+    rotation_range=5,
+    width_shift_range=0.01,
+    height_shift_range=0.01,
+    shear_range=0.01,
+    zoom_range=0.01,
     horizontal_flip=True,
     fill_mode='nearest'
 )
---
+++ utils.py
@@ -0,0 +1,38 @@
+import pickle
+
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+
+def conf_matrix(true_labels, pred_labels, classes):
+    cm = confusion_matrix(true_labels, pred_labels)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="magma", xticklabels=classes, yticklabels=classes)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+
+def analyze_train_history(train_history):
+    plt.figure(figsize=(15, 10))
+
+    plt.subplot(2, 1, 1)
+    plt.plot(train_history['loss'])
+    plt.plot(train_history['val_loss'], 'ro')
+    plt.title('Loss')
+    plt.grid(True)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(train_history['accuracy'])
+    plt.plot(train_history['val_accuracy'], 'ro')
+    plt.title('Accuracy')
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    with open('experiment1/models/train_history_raw.pkl', 'rb') as file:
+        train_hist = pickle.load(file)
+        analyze_train_history(train_hist)
